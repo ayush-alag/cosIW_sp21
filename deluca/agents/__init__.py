54babad0be7a7ad3e80eeb310c98367d7c83e511
Ayush Alag <aalag@princeton.edu>
2021-04-09 00:56:07 -0400

    trying to connect to env

--- deluca/agents/__init__.py
+++ deluca/agents/__init__.py
@@ -20,5 +20,6 @@ from deluca.agents._zero import Zero
 from deluca.agents._drc import DRC
 from deluca.agents._adaptive import Adaptive
 from deluca.agents._deep import Deep
+from deluca.agents._mem import Mem
 
-__all__ = ["LQR", "PID", "GPC", "ILQR", "Hinf", "Zero", "DRC", "Adaptive", "Deep"]
+__all__ = ["LQR", "PID", "GPC", "ILQR", "Hinf", "Zero", "DRC", "Adaptive", "Deep", "Mem"]
--- deluca/agents/_mem.py
+++ deluca/agents/_mem.py
@@ -1,3 +1,30 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""deluca.agents._mem"""
+from numbers import Real
+from typing import Callable
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax import grad
+from jax import jit
+
+from deluca.agents._lqr import LQR
+from deluca.agents.core import Agent
+
+from deluca.utils.optimR import optimROBD
 '''
 Not Used
 
@@ -132,8 +159,8 @@ class Mem(Agent):
         return self._ys[self._t] - lsum
     
     # takes in state, disturbance radius, qs, lambda
-    def __call__(self, x_t, radius_t=1, q_t=None, lam=0):
+    def __call__(self, state, radius_t=1, q_t=None, lam=0):
         if q_t == None:
             q_t = np.ones((self._p+self._T))
-        self._xs[self._t] = x_t # add it to the list
+        self._xs[self._t] = state # add it to the list
         return self.controlAlgo(radius_t, q_t, lam)
